8c535211c88bf658a264e3c68ccba28ee66c5c9a
Sean Rayment <sean_rayment@brown.edu>
2020-04-22 22:33:22 -0400

    createing documents working

--- backend/documents/serializers.py
+++ backend/documents/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 from .models import Document
 from authentication.serializers import CustomUserSerializer
+from authentication.models import CustomUser
 
 class DocumentSerializer(serializers.ModelSerializer):
-    owner = CustomUserSerializer()
+    owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Document
--- backend/documents/views.py
+++ backend/documents/views.py
@@ -6,17 +6,26 @@ from authentication.models import CustomUser
 from rest_framework.response import Response
 from django.http import Http404
 from .serializers import DocumentSerializer
+from authentication.serializers import CustomUserSerializer
+import json
 
 class DocumentListView(APIView):
 
     def get(self, request, format='json'):
         try:
-            user = CustomUser.objects.get(username=request.user.username)
-            docs = user.document_set.filter(is_deleted=False)
+            docs = request.user.document_set.filter(is_deleted=False)
             serializer = DocumentSerializer(docs, many=True)                
             return Response(serializer.data)
         except:
             raise Http404
+
+    def post(self, request, format='json'):
+        serializer = DocumentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class DocumentView(APIView):
 
@@ -29,6 +38,7 @@ class DocumentView(APIView):
     def get(self, request, pk, format='json'):
         try:
             user = CustomUser.objects.get(username=request.user.username)
+            print(user)
             doc = self.get_document(pk)
             if doc.owner == user:
                 serializer = DocumentSerializer(doc)
